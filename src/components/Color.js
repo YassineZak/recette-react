7c954b5d851ca772b3f2cebd74be89ab1f72db21
YassineZak <yassine.zakari@hotmail.fr>
2019-04-28 21:06:38 +0200

    personnalisation couleur pas fini

--- src/components/Color.js
+++ src/components/Color.js
@@ -8,36 +8,17 @@ export default class ColorProvider extends Component {
         color: '#119c62',
         showPalette: true
     }
-    handleClick = () => {
-      
-      if(this.state.showPalette){
-          this.setState({showPalette: false})
-      }
-      else{
-        this.setState({showPalette: true})
-      }
-    }
 
-    handleChangeComplete = color => {
-     this.setState({ color: color.hex })
-      console.log(this.state)
-    }
+    
 
   render() {
     return (
-      <Fragment>
-        {
-          this.state.showPalette ? <SketchPicker color={ this.state.background }
-          onChange={ this.handleChangeComplete } /> : null
-          
-        }
       <ColorContext.Provider
       value={{state: this.state}}
       >
         {this.props.children}
         
       </ColorContext.Provider>
-      </Fragment>
     )
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,20 +6,38 @@ export default class Header extends Component {
 
 
     state = {
-        showPalette: false,
+        showPalette: true,
         background: '#fff'
     }
 
-  formatPseudo = pseudo =>
-    /[aeiou]/i.test(pseudo[0]) ? `d'${pseudo}` : `de ${pseudo}`;
+    handleChangeComplete = color => {
+        this.setState({ color: color.hex })
+       }
+
+    formatPseudo = pseudo =>
+        /[aeiou]/i.test(pseudo[0]) ? `d'${pseudo}` : `de ${pseudo}`;
+
+    handleClick = () => {
+        if (this.state.showPalette) {
+            this.setState({showPalette : false})
+        }
+        else{
+            this.setState({showPalette : true})
+        }
+    }
 
   
 
   render() {
+
         return (
             <ColorContext.Consumer color={this.state.background} showPalette={this.state.showPalette}>
             {context => (
-                <header style={{ backgroundColor: context.state.color }} onClick = {this.handleClick}>
+                context.state.showPalette = this.state.showPalette,
+                context.state.color = this.state.color,
+                <header style={{ backgroundColor: context.state.color }} onClick = {() => this.handleClick()}>
+                {context.state.showPalette ? <SketchPicker color={ this.state.background }
+                onChange={ this.handleChangeComplete } /> : null }
                 <h1>La boîte à recettes {this.formatPseudo(this.props.pseudo)}</h1>
                 </header>
             )}
